0a50b2a6d9457036d655bcc9eb75fccd7acb636d
desoga10 <done2stay@gmail.com>
2023-03-25 02:08:35 +0100

    Made Updates

---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+  cloud_name: 'duzvjyvo1',
+  api_key: '733616477884532',
+  api_secret: 'lAFTipvMS4sILKcZBHllLkMVg9k'
+})
+
+module.exports = cloudinary
